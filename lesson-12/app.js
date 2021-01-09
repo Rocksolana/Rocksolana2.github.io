c326f0c0d9b8d6964bc8eb0ea14c5410a977daa2
ВАШЕ_ИМЯ <tsypuk@gmail.com>
2021-01-09 16:20:05 +0200

    created app.js

---
+++ lesson-12/app.js
@@ -0,0 +1,22 @@
+//Завдання1
+/*let number = prompt('введіть число','');
+number=number*number;
+alert(`${number}`);*/
+
+
+//Завдання2
+
+let Name=prompt('введіть імя','');
+let deposit=prompt('введіть суму','');
+let month=prompt('введіть місяць','');
+let percent=0.2;
+summa=(1+percent/12)**month;
+summa=summa*deposit;
+
+alert(`Шановний ${Name} ви внесли суму ${deposit} під ${percent} річних, на термін ${month} місяців. За цей період часу Ви заробите ${summa}`);
+
+
+
+//Завдання3
+
+
